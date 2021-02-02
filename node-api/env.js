10426dd6279182e36fed6d3ddb60c6562af0fc86
José Posada <jose.posadam@perficient.com>
2021-02-02 17:37:11 -0500

    Added base Node API

---
+++ node-api/env.js
@@ -0,0 +1,4 @@
+exports.API_PORT = process.env.API_PORT || 3000;
+
+exports.AUTH_API_URL = process.env.AUTH_API_URL || 'auth.api.com';
+exports.UNNAMED_API_URL = process.env.UNNAMED_API_URL || 'other.api.com';
---
+++ node-api/server.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+const express = require('express');
+const env = require('./env');
+const axios = require('axios');
+
+const app = express();
+
+const getToken = (req) => {
+  const bearerHeader = req.headers['authorization'];
+
+  if (bearerHeader) {
+    const bearer = bearerHeader.split(' ');
+    return bearer[1];
+  }
+
+  return ''
+}
+
+const authenticate = async (token) => {
+  try {
+    await axios.get(`${env.AUTH_API_URL}/auth/${token}`);
+  } catch (error) {
+    throw {
+      error,
+      message: 'Authentication failed'
+    }
+  }
+}
+
+app.get('/', function(req, res){
+  var response = {
+    message : 'Ok'
+  }
+  res.json(response);
+})
+
+
+app.get('/products', async (req, res) => {
+  try {
+    const token = getToken(req);
+    await authenticate(token);
+    const products = await axios.get(`${env.UNNAMED_API_URL}/products`);
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+app.post('/payments', async (req, res) => {
+  try {
+    const token = getToken(req);
+    await authenticate(token);
+    const payment = await axios.post(`${env.UNNAMED_API_URL}/payments`);
+    res.status(200).json(payment);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+console.log(`Server listening port: ${env.API_PORT}`);
+app.listen(env.API_PORT);
+module.exports = app;
