b1bea22468944a78c73bcee1542e02de0139c088
José Posada <jose.posadam@perficient.com>
2021-04-01 13:48:01 -0500

    Nodejs api integration

--- node-api/env.js
+++ node-api/env.js
@@ -1,4 +1,4 @@
 exports.API_PORT = process.env.API_PORT || 3000;
 
-exports.AUTH_API_URL = process.env.AUTH_API_URL || 'auth.api.com';
-exports.UNNAMED_API_URL = process.env.UNNAMED_API_URL || 'other.api.com';
+exports.AUTH_API_URL = process.env.AUTH_API_URL || 'http://localhost:8000';
+exports.EMPLOYEES_API_URL = process.env.EMPLOYEES_API_URL || 'http://localhost:8085';
--- node-api/server.js
+++ node-api/server.js
@@ -1,63 +1,103 @@
-var apm = require('elastic-apm-node').start({
-  serviceName: 'node-api',
-  serverUrl: 'http://apm-server:8200'
-})
+var apm = require("elastic-apm-node").start({
+  serviceName: "node-api",
+  serverUrl: "http://apm-server:8200",
+});
 
-require('dotenv').config();
-const express = require('express');
-const env = require('./env');
-const axios = require('axios');
+require("dotenv").config();
+const express = require("express");
+const env = require("./env");
+const axios = require("axios");
+const FormData = require("form-data");
 
 const app = express();
 
-const getToken = (req) => {
-  const authHeader = req.headers['authorization'];
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-  if (authHeader) {
-    return authHeader.replace('Basic ', '');
-  }
-  return ''
-}
+const authenticate = async (username, password) => {
+  const form = new FormData();
+  form.append("username", username);
+  form.append("password", password);
+  const response = await axios.post(`${env.AUTH_API_URL}/auth/`, form, {
+    headers: form.getHeaders(),
+  });
+  console.log(response.data);
+  return response.data;
+};
 
-const authenticate = async (token) => {
-  try {
-    await axios.get(`${env.AUTH_API_URL}/auth/${token}`);
-  } catch (error) {
-    throw {
-      error,
-      message: 'Authentication failed',
-      code: 401,
-    }
-  }
-}
+const getUser = async (req) => {
+  const token = req.header("authorization");
+  const response = await axios.get(`${env.AUTH_API_URL}/users/1`, {
+    headers: { Authorization: token || "" },
+  });
+  return response.data;
+};
 
-app.get('/', function(req, res){
+app.get("/", function (req, res) {
   var response = {
-    message : 'Ok'
-  }
+    message: "Ok",
+  };
   res.json(response);
-})
+});
 
-
-app.get('/products', async (req, res) => {
+app.post("/login", async (req, res) => {
   try {
-    const token = getToken(req);
-    await authenticate(token);
-    const products = await axios.get(`${env.UNNAMED_API_URL}/products`);
-    res.status(200).json(products);
+    const response = await authenticate(req.body.username, req.body.password);
+    res.status(200).json(response);
   } catch (error) {
-    res.status(error.code).json(error);
+    const response = error.response;
+    if (response) return res.status(response.status).json(response.data);
+    return res.status(500).json(error);
   }
 });
 
-app.post('/payments', async (req, res) => {
+app.get("/employees/:employeeId", async (req, res) => {
   try {
-    const token = getToken(req);
-    await authenticate(token);
-    const payment = await axios.post(`${env.UNNAMED_API_URL}/payments`);
-    res.status(200).json(payment);
+    const { employeeId } = req.params;
+    await getUser(req);
+    const employee = await axios.get(
+      `${env.EMPLOYEES_API_URL}/employee/${employeeId}`
+    );
+    res.status(200).json(employee.data);
   } catch (error) {
-    res.status(error.code).json(error);
+    const response = error.response;
+    if (response) return res.status(response.status).json(response.data);
+    return res.status(500).json(error);
+  }
+});
+
+app.get("/employees", async (req, res) => {
+  try {
+    await getUser(req);
+    const employees = await axios.get(
+      `${env.EMPLOYEES_API_URL}/employee/allemployees`
+    );
+    res.status(200).json(employees.data);
+  } catch (error) {
+    const response = error.response;
+    if (response) return res.status(response.status).json(response.data);
+    return res.status(500).json(error);
+  }
+});
+
+app.post("/employees", async (req, res) => {
+  try {
+    const { id, name, role, salary } = req.body;
+    await getUser(req);
+    const employee = await axios.post(
+      `${env.EMPLOYEES_API_URL}/employee/create`,
+      {
+        id,
+        name,
+        role,
+        salary,
+      }
+    );
+    res.status(200).json(employee.data);
+  } catch (error) {
+    const response = error.response;
+    if (response) return res.status(response.status).json(response.data);
+    return res.status(500).json(error);
   }
 });
 
