80b29086f31644c36ab744d1cf357d7793f9244a
julammz <julammz@gmail.com>
2021-04-04 19:36:12 -0500

    Delete java-api directory

--- java-api/src/main/java/org/perficient/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package org.perficient.repository;
-
-import org.perficient.entity.Employee;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EmployeeRepository extends MongoRepository<Employee, Integer> {
-}
